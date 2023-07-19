7c3b47d8b9d6336f1be9c758ac4628ffae214296
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-19 21:04:22 +0200

    Creating input components

---
+++ frontend/src/components/InputForm.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function InputForm({ state, setter, type, placeholder }) {
+  const maxLength = 50;
+  const handleChangeForm = (event) => {
+    if (event.target.value.length <= maxLength) {
+      setter(event.target.value);
+    }
+  };
+  return (
+    <div className="input-Form-container">
+      <input
+        className="input-Form"
+        type={type}
+        required
+        value={state}
+        onChange={handleChangeForm}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
+InputForm.propTypes = {
+  state: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  setter: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
+export default InputForm;
