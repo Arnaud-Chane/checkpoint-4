8644e99cf5ccf564cc0892270019cd818a47ab23
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-19 21:02:10 +0200

    Adding structure

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-function Login() {
-  return <div>Login</div>;
-}
+export default function Login() {
+  const handleSubmit = async () => {
+    console.info("test");
+  };
 
-export default Login;
+  return (
+    <div className="login-container-page">
+      <h2>Accéder à votre compte</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input />
+
+        <input />
+      </form>
+      <div className="links-page-login">
+        <div className="buttons-container-login-page">
+          <button className="primary-button" type="submit">
+            Connexion
+          </button>
+
+          <p>Inscription</p>
+        </div>
+
+        <p>Mot de passe oublié ?</p>
+      </div>
+    </div>
+  );
+}
