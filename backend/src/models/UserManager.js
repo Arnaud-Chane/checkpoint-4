4487aeca0e92335a68dbf81addcbeceb628a2a2a
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-19 21:16:40 +0200

    Cleaning items backend elements & adding User Manager

---
+++ backend/src/models/UserManager.js
@@ -0,0 +1,36 @@
+const AbstractManager = require("./AbstractManager");
+
+class UserManager extends AbstractManager {
+  constructor() {
+    super({ table: "user" });
+  }
+
+  addUser(user) {
+    return this.database.query(
+      `insert into ${this.table} ( email, password, is_admin ) values (?, ?, ?)`,
+      [user.email, user.password, user.is_admin]
+    );
+  }
+
+  updateUser(user) {
+    return this.database.query(
+      `UPDATE ${this.table} SET email = ? WHERE user_id = ?`,
+      [user.email]
+    );
+  }
+
+  updateAdmin(user) {
+    return this.database.query(
+      `update ${this.table} set is_admin = ? where user_id = ?`,
+      [user.is_admin, user.user_id]
+    );
+  }
+
+  findUserByEmail(user) {
+    return this.database.query(`select * from ${this.table} where email = ?`, [
+      user.email,
+    ]);
+  }
+}
+
+module.exports = UserManager;
