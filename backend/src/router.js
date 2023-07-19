0853936efe137677333f638adc04d8b802dd599e
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-19 21:28:24 +0200

    Adding routes for users - getAll, getById, update, delete, update to Admin, get info for later

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+const userControllers = require("./controllers/userControllers");
+
+router.post("/users", userControllers.createUser);
+
+router.get("/users", userControllers.getAllUsers);
+router.get("/users/:id", userControllers.getUserById);
+router.put("/users/:id", userControllers.updateUser);
+router.delete("/userinformation/:id", userControllers.destroy);
+router.put("/isadmin/:id", userControllers.updateIsAdmin);
+router.delete("/users/:id", userControllers.destroy);
+router.post("/userinformation", userControllers.getUserInformation);
+
 module.exports = router;
