f899abab9038c995d83e6eaa55557f3b8b371b68
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-21 16:07:07 +0200

    Changing all hashed password in sql

--- backend/sql/checkpoint4.sql
+++ backend/sql/checkpoint4.sql
@@ -20,12 +20,12 @@ CREATE TABLE `tasks` (
 
 INSERT INTO users (email, password, is_admin)
 VALUES 
-  ('valerie.apert@example.com', "$argon2id$v=19$m=65536,t=5,p=1$7y5DipbF2biofOLgY38Eyg$WZ8mz2svq5bYZOjVsoYgFXkapyifGXdR7tplz+tOlnw", 0),
-  ('alice.johnsie@example.com', "$argon2id$v=19$m=65536,t=5,p=1$7y5DipbF2biofOLgY38Eyg$WZ8mz2svq5bYZOjVsoYgFXkapyifGXdR7tplz+tOlnw", 0),
-  ('robert.brown@example.com', "$argon2id$v=19$m=65536,t=5,p=1$7y5DipbF2biofOLgY38Eyg$WZ8mz2svq5bYZOjVsoYgFXkapyifGXdR7tplz+tOlnw", 0),
-  ('julia.lee@example.com', "$argon2id$v=19$m=65536,t=5,p=1$7y5DipbF2biofOLgY38Eyg$WZ8mz2svq5bYZOjVsoYgFXkapyifGXdR7tplz+tOlnw", 0),
-  ('alexandre.moreau@example.com', "$argon2id$v=19$m=65536,t=5,p=1$7y5DipbF2biofOLgY38Eyg$WZ8mz2svq5bYZOjVsoYgFXkapyifGXdR7tplz+tOlnw", 0),
-  ('test@gmail.com', "$argon2id$v=19$m=65536,t=5,p=1$7y5DipbF2biofOLgY38Eyg$WZ8mz2svq5bYZOjVsoYgFXkapyifGXdR7tplz+tOlnw", '1');
+  ('valerie.apert@example.com', "$argon2id$v=19$m=65536,t=5,p=1$+rX0+tbD6WFYqAiz8HHMIQ$8i3zbqOEx1OcsxPG8lgeXQ6MeRtStrcgRbN7pqCTAy8", 0),
+  ('alice.johnsie@example.com', "$argon2id$v=19$m=65536,t=5,p=1$+rX0+tbD6WFYqAiz8HHMIQ$8i3zbqOEx1OcsxPG8lgeXQ6MeRtStrcgRbN7pqCTAy8", 0),
+  ('robert.brown@example.com', "$argon2id$v=19$m=65536,t=5,p=1$+rX0+tbD6WFYqAiz8HHMIQ$8i3zbqOEx1OcsxPG8lgeXQ6MeRtStrcgRbN7pqCTAy8", 0),
+  ('julia.lee@example.com', "$argon2id$v=19$m=65536,t=5,p=1$+rX0+tbD6WFYqAiz8HHMIQ$8i3zbqOEx1OcsxPG8lgeXQ6MeRtStrcgRbN7pqCTAy8", 0),
+  ('alexandre.moreau@example.com', "$argon2id$v=19$m=65536,t=5,p=1$+rX0+tbD6WFYqAiz8HHMIQ$8i3zbqOEx1OcsxPG8lgeXQ6MeRtStrcgRbN7pqCTAy8", 0),
+  ('test@gmail.com', "$argon2id$v=19$m=65536,t=5,p=1$+rX0+tbD6WFYqAiz8HHMIQ$8i3zbqOEx1OcsxPG8lgeXQ6MeRtStrcgRbN7pqCTAy8", '1');
 
 INSERT INTO tasks (name, user_id) 
 VALUES 
