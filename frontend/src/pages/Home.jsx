5bbcca821b523ccb76131bccfd8b51172af9a940
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-22 15:23:22 +0200

    Retrieving the id by context and getting the right url for back

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,15 +5,18 @@ import { UserInfoContext } from "../context/UserInfoContext";
 function Home() {
   const { userInfo } = useContext(UserInfoContext);
 
+  const [userId, setUserId] = useState("");
   const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    setUserId(userInfo.userId);
+  }, []);
 
   useEffect(() => {
     const fetchTask = async () => {
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/api/users/${
-            userInfo.user_id
-          }/tasks`
+          `${import.meta.env.VITE_BACKEND_URL}/api/users/${userId}/tasks`
         );
         setTaskList(response.data);
       } catch (error) {
@@ -21,7 +24,7 @@ function Home() {
       }
     };
     fetchTask();
-  }, []);
+  }, [userId]);
 
   return (
     <div className="HomeAdminListingtask">
