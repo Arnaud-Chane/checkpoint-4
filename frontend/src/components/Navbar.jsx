fc1a167c161b7621f5abb4b2b708525aaac3fc58
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-22 16:14:12 +0200

    Adding the logout button to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
 function Navbar() {
+  const logout = () => {
+    localStorage.removeItem("token");
+  };
+
   return (
-  <div className="Navbar">Navbar</div>
+    <div className="Navbar">
+      <a href="/login">
+        <button
+          type="submit"
+          className="link-deconnection-admin"
+          onClick={logout}
+        >
+          Déconnexion
+        </button>
+      </a>
+    </div>
   );
 }
 
