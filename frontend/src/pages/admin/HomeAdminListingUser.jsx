c2a72339889264669f5a93048435a864efd9a32d
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-22 11:42:03 +0200

    fetching all user useEffect

--- frontend/src/pages/admin/HomeAdminListingUser.jsx
+++ frontend/src/pages/admin/HomeAdminListingUser.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function HomeAdminListingUser() {
-  return <div>HomeAdminListingUser</div>;
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/users`
+        );
+        setUserList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  console.info(userList);
+
+  return <div>Admin page</div>;
 }
 
 export default HomeAdminListingUser;
