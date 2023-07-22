de709c53bd7bff45ef4524b9ccaaf0b6df8e4511
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-22 14:47:07 +0200

    Retrieve all tasks

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Home() {
-  return <div>Home</div>;
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    const fetchTask = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/tasks`
+        );
+        setTaskList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchTask();
+  }, []);
+
+  return (
+    <div className="HomeAdminListingtask">
+      <ul className="task-list">
+        {taskList.map((task) => {
+          return (
+            <li className="task" key={task.task_id}>
+              {task.name}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
