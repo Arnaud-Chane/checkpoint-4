ae3d3b72cdb1ac740b44223e3db891afe9d070b5
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-22 11:21:40 +0200

    Modif user by task in index models

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -35,8 +35,8 @@ const TaskManager = require("./TaskManager");
 models.user = new UserManager();
 models.user.setDatabase(pool);
 
-models.user = new TaskManager();
-models.user.setDatabase(pool);
+models.task = new TaskManager();
+models.task.setDatabase(pool);
 
 // bonus: use a proxy to personalize error message,
 // when asking for a non existing model
