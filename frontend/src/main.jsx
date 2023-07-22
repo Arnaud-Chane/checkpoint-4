c554fc9db20338c34f8128af04e8a7f5982752bc
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-22 15:09:49 +0200

    Adding context provider to main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,8 @@ import {
 import PrivateRoutes from "./utils/PrivateRoutes";
 import userRoles from "./utils/constantRoles";
 
+import UserInfoProvider from "./context/UserInfoContext";
+
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
@@ -73,6 +75,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <UserInfoProvider>
+      <RouterProvider router={router} />
+    </UserInfoProvider>
   </React.StrictMode>
 );
